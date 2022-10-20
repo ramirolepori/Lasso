34788a1e2cbd7f0f503e5ee6fd9a0d2fc9d0fb9b
ramirolepori <leporiramiro@gmail.com>
2022-10-20 16:21:17 -0300

    ObjectToCsv en generadorUbicaciones

--- generadorUbicacionesjs.js
+++ generadorUbicacionesjs.js
@@ -1,23 +1,26 @@
 const ObjectsToCsv = require('objects-to-csv');
+let value2 = "";
+let value3 = "";
+let currentdate = new Date();
+let datetime = "";
 
 function generateRandomDecimalInRangeFormatted(min, max, places) {
   let value = Math.random() * (max - min + 1) + min;
   return Number.parseFloat(value).toFixed(places);
 }
 function editUbicacion(ub, places) {
-  let value = ub + Math.random() * (0.000025 + 0.000025) - 0.000025;
+  let value = ub + (Math.random() * (0.000025 + 0.000025) - 0.000025);
   return Number.parseFloat(value).toFixed(places);
 }
 
 function generadorUbicaciones() {
   var foo = [];
 
-  let value2 = "";
-  let value3 = "";
 
-  for (var i = 10001; i <= 10500; i++) {
-    var currentdate = new Date();
-    var datetime =
+
+  for (let i = 10001; i <= 10500; i++) {
+    currentdate = new Date();
+    datetime =
       currentdate.getDate() +
       "/" +
       (currentdate.getMonth() + 1) +
@@ -42,12 +45,14 @@ function generadorUbicaciones() {
   }
   return foo;
 }
-var ubicaciones = [];
+let ubicaciones = [];
 ubicaciones.push(generadorUbicaciones());
+//console.log(ubicaciones)
 
-for (var i = 0; i <= 10; i++) {
-  var foo = [];
-  for (var j = 0; j <= ubicaciones[i].length; j++) {
+for (let i = 0; i <= 10; i++) {
+  let foo = [];
+  for (let j = 0; j < ubicaciones[i].length; j++) {
+    //console.log(ubicaciones[i][j])
     foo.push({
       idSensor: ubicaciones[i][j].idSensor,
       idVaca: ubicaciones[i][j].idVaca,
@@ -56,16 +61,17 @@ for (var i = 0; i <= 10; i++) {
       dataTime: datetime,
     });
   }
+  (async () => {
+    const csv = new ObjectsToCsv(foo);
+   
+    // Save to file:
+    await csv.toDisk('./test.csv',{append:true});
+   
+    // Return the CSV file as string:
+    //console.log(await csv.toString());
+  })();
   ubicaciones.push(foo);
 }
 
 
-(async () => {
-  const csv = new ObjectsToCsv(ubicaciones);
- 
-  // Save to file:
-  await csv.toDisk('/test.csv');
- 
-  // Return the CSV file as string:
-  console.log(await csv.toString());
-})();
+
