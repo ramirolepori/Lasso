67a1615be1d7c580de8319b918089c11929ea01b
kbza94 <manuelcabezarivarola@gmail.com>
2022-10-27 10:40:09 -0300

    Numerosos valores convertidos a parámetros

--- generadorUbicacionesV2.js
+++ generadorUbicacionesV2.js
@@ -1,20 +1,25 @@
 const ObjectsToCsv = require('objects-to-csv');
 
-function generateRandomDecimalInRangeFormatted(min, max, places) {
+function generateRandomDecimalInRangeFormatted(min, max, places = 6) {
     let value = Math.random() * (max - min + 1) + min;
     return Number.parseFloat(value).toFixed(places);
 }
 
-function editarUbicacion(ub, places) {
-    let value = Number(ub) + (Math.random() * (0.000025 + 0.000025) - 0.000025);
+function editarUbicacion(ub, places = 6, min = 0.000025, max = 0.000025) {
+    let value = Number(ub) + (Math.random() * (max - min) + min);
     let result = Number.parseFloat(value).toFixed(places);
     return result;
 }
 
-function generadorUbicaciones(id_min, id_max) {
+function nuevaListaUbicaciones(id_min = 0, id_max = 500,
+     lat_min = -31.835612, lat_max = -31.952209,
+     lon_min = -62.272098, lon_max = -62.472255) {
+    // Genera una lista de ubicaciones aleatorias asociadas a ids.
+    // La lista va desde id_min hasta id_max inclusive
+    // 
     let foo = [];
-    let value2 = "";
-    let value3 = "";
+    let latitud;
+    let longitud;
   
     for (let i = id_min; i <= id_max; i++) {
       currentdate = new Date();
@@ -30,21 +35,21 @@ function generadorUbicaciones(id_min, id_max) {
         currentdate.getMinutes() +
         ":" +
         currentdate.getSeconds();
-      value2 = generateRandomDecimalInRangeFormatted(-31.835612, -31.952209, 6);
-      value3 = generateRandomDecimalInRangeFormatted(-62.272098, -62.472255, 6);
+      latitud = generateRandomDecimalInRangeFormatted(lat_min, lat_max);
+      longitud = generateRandomDecimalInRangeFormatted(lon_min, lon_max);
   
       foo.push({
         idSensor: i,
         idVaca: i,
-        ubicacionX: value2,
-        ubicacionY: value3,
+        ubicacionX: latitud,
+        ubicacionY: longitud,
         dataTime: datetime,
       });
     }
     return foo;
 }
 
-function nuevaListaUbicaciones(ub){
+function modificarListaUbicaciones(ub){
     let nueva_lista = []
     for (let i = 0; i < ub.length; i++) {
         let nueva_ubicacionX = editarUbicacion(ub[i].ubicacionX, 6);
@@ -61,8 +66,8 @@ function nuevaListaUbicaciones(ub){
     return nueva_lista;
 }
 
-let ubicaciones = generadorUbicaciones(10, 20);
-let ubicaciones2 = nuevaListaUbicaciones(ubicaciones);
+let ubicaciones = nuevaListaUbicaciones(10, 20);
+let ubicaciones2 = modificarListaUbicaciones(ubicaciones);
 
 for(let i = 0; i < ubicaciones.length; i++){
     console.log("UbicacionX A: " + ubicaciones[i].ubicacionX + " UbicacionX B: " + ubicaciones2[i].ubicacionX );
