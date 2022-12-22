73bae0161b5c60272dab5fd458690af94285839e
ramirolepori <leporiramiro@gmail.com>
2022-12-22 14:10:22 -0300

    Inicio desarrollo backend

---
+++ Backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const morgan=require('morgan');
+
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2)
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//Nuestro primer WS Get
+// app.get('/', (req, res) => {    
+//     res.json(
+//         {
+//             "Title": "Hola mundo"
+//         }
+//     );
+// })
+
+//Routes
+app.use(require('./routes/index'));
+app.use('./cows.json', require('./routes/cows'));
+
+
+//Iniciando el servidor, escuchando...
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
+
---
+++ Backend/src/routes/cows.js
@@ -0,0 +1,55 @@
+const { Router, request } = require('express');
+const router = Router();
+const _ = require('underscore');
+
+const data = require('../cows.json');
+
+//Cows
+router.get('/', (req, res) => {
+    res.json(data);
+});
+
+router.post('/', (req, res) => {
+    const { long, lat, dateTime } = req.body;
+    if (long && lat && dateTime) {
+        const idSensor = data.length + 1;
+        const idCow = data.length + 1;
+        const newCow = { ...req.body, idSensor, idCow };
+        data.push(newCow);
+        res.json(data);
+    }
+    else {
+        res.status(500).json({ "error": "There was an error." });
+    }
+});
+
+router.put('/:idCow', (req, res) => {
+    const { idCow } = req.params;
+    const { long, lat, dateTime } = req.body;
+    if (long && lat && dateTime) {
+        _.each(data, (cow, i) => {
+            if (cow.idCow == idCow) {
+                cow.idSensor = idSensor
+                cow.long = long;
+                cow.lat = lat;
+                cow.dateTime = dateTime;
+            }
+        });
+        res.json(data);
+    }
+    else {
+        res.status(500).json({ "error": "There was an error." });
+    }
+});
+
+router.delete('/:idCow', (req, res) => {
+    const { idCow } = req.params;
+    _.each(data, (cow, i) => {
+        if (cow.idCow == idCow) {
+            data.splice(i, 1);
+        }
+    });
+    res.send(data);
+})
+
+module.exports = router;
