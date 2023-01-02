94a5dbb774102b864515c9a59b5ef3bff8f00d2f
ramirolepori <leporiramiro@gmail.com>
2023-01-02 12:47:05 -0300

    Actualizo backend

--- Backend/src/lib/querys.js
+++ Backend/src/lib/querys.js
@@ -1,31 +1,30 @@
-let XMLHttpRequest = require('xhr2');
+let XMLHttpRequest = require("xhr2");
 
-
-function newToken(){
-//convierto la respuesta en string y la guardo en la variable response 
-let response = qRequest('POST','https://iam.cloud.ibm.com/identity/token', 'string')
-//busco el indice en donde termina el token
-let indice = response.indexOf('"refresh_token":"') - 2;
-//busco el token y lo guardo en la variable token
-let bearer = 'Bearer ';
-let token = bearer.concat(response.substring(17, indice));
-//guardo el token en la variable por defecto
-return token
+function newToken() {
+  //convierto la respuesta en string y la guardo en la variable response
+  let response = qRequest(
+    "POST",
+    "https://iam.cloud.ibm.com/identity/token",
+    "string"
+  );
+  //busco el indice en donde termina el token
+  let indice = response.indexOf('"refresh_token":"') - 2;
+  //busco el token y lo guardo en la variable token
+  let bearer = "Bearer ";
+  let token = bearer.concat(response.substring(17, indice));
+  //guardo el token en la variable por defecto
+  return token;
 }
-
-
 
 function qRequest(typeQuery, url, responseType, header) {
   const xhr = new XMLHttpRequest();
   let data;
   xhr.open(typeQuery, url);
-
-  xhr.setRequestHeader(header[0]['nameHeader'], header[0]['valueHeader']);
+  xhr.setRequestHeader(header[0]["nameHeader"], header[0]["valueHeader"]);
   xhr.send();
   xhr.responseType = responseType;
   xhr.onload = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      
       data = xhr.response;
       console.log(data);
     } else {
@@ -36,8 +35,7 @@ function qRequest(typeQuery, url, responseType, header) {
   return data;
 }
 
-
 module.exports = {
-    "qRequest": qRequest,
-    "newToken": newToken
-}
+  qRequest: qRequest,
+  newToken: newToken,
+};
