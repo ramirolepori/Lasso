8c2fc8c787a1649fca44d708996b4b5a02880f42
ramirolepori <leporiramiro@gmail.com>
2023-01-06 11:40:04 -0300

    Borro archivos basura

--- Backend/src/borrarEsto.js
+++
@@ -1,6 +0,0 @@
-const querys = require('../src/lib/querys');
-
-
-const header = [{'Authorization': 'Basic Yng6Yng'},
-                {'Content-Type': 'application/x-www-form-urlencoded'}];
-querys.qRequest('POST', 'https://iam.cloud.ibm.com/identity/token', 'string', header)
