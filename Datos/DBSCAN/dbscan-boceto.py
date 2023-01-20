d6ffbc12474253cc273d6562d0e0ce4587d4fe0b
kbza94 <manuelcabezarivarola@gmail.com>
2023-01-20 08:56:34 -0300

    Script de DBSCAn puede leer archivos JSON

--- Datos/DBSCAN/dbscan-boceto.py
+++ Datos/DBSCAN/dbscan-boceto.py
@@ -4,31 +4,40 @@ from sklearn.cluster import DBSCAN
 from sklearn import metrics
 import matplotlib.pyplot as plt
 import numpy as np
+import json
+
+def read_json_file(filepath = 'Datos\\boceto-vaca.json'):
+    """ Lee un archivo JSON ubicado en 'filepath'
+    Convierte el archivo a un dict
+    """
+    f = open(filepath)
+    d = json.load(f)
+    f.close()
+    return d
 
 def vacas_dbscan(vacas, eps = 0.43):
-    """Esta funcion toma un arreglo de vacas, las procesa y ejecuta
+    """Esta funcion toma un dict de vacas, las procesa y ejecuta
     DBSCAN sobre ellas"""
-    # Aquí se generaria la lista que contiene solo ubicaciones
-    vacas_aisladas = []
     ubicaciones = []
-    clusters, labels = _vacas_dbscan(ubicaciones, eps)
+    vacas_ids = list(vacas.keys())
+    for id in vacas_ids:
+        latitud = vacas[id]["ubicaciones"][0]["lat"]
+        longitud = vacas[id]["ubicaciones"][0]["long"]
+        ubicaciones.append([latitud, longitud])
+
+    clusters, labels = _dbscan(ubicaciones, eps)  
+
+    vacas_aisladas = []
     for i in range(len(labels)):
-        if labels[i] is -1:
-            vacas_aisladas.append(vacas[i].id) # Es una vaca aislada
+        if labels[i] == -1:
+            vacas_aisladas.append(vacas_ids[i]) # Es una vaca aislada
     
     return vacas_aisladas # Debería retornar otra estructura de datos
 
-
-def _vacas_dbscan(vacas, eps = 0.43):
+def _dbscan(ubicaciones, eps = 0.43, min_samples = 2):
     """
-    Generamos puntos. La idea es tomar los puntos de 'vacas' pero por el momento
-    los generamos nostros
     """
-    centers = [[1, 1], [-1, -1], [1, -1]]
-    X, labels_true = make_blobs(
-        n_samples=75, centers=centers, cluster_std=0.4, random_state=0
-    )
-    db = DBSCAN(eps, min_samples=10).fit(X)
+    db = DBSCAN(eps, min_samples).fit(ubicaciones)
     labels = db.labels_
 
     # Number of clusters in labels, ignoring noise if present.
@@ -43,3 +52,7 @@ def _vacas_dbscan(vacas, eps = 0.43):
             clusters[l] = 1
 
     return clusters, labels
+
+vacas = read_json_file('Datos\\boceto-vaca.json')
+print("Vacas aisladas: ", vacas_dbscan(vacas))
+#print(vacas["1"]["ubicaciones"][0]["lat"])
